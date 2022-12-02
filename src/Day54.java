d5f9b59d1021739c7784332e80043d4d51138d75
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2022-12-02 23:20:11 +0530

    Check if it is possible to convert one string into another with given constraints

---
+++ src/Day54.java
@@ -0,0 +1,43 @@
+class Solution {
+    int isItPossible(String S, String T, int M, int N) {
+        if (M != N) return 0;
+
+        int a = 0, b = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (S.charAt(i) == 'A') a++;
+            else if (T.charAt(i) == 'B') b++;
+
+            if (T.charAt(i) == 'A') a--;
+            else if (T.charAt(i) == 'B') b--;
+        }
+
+        if (a != 0 || b != 0) return 0;
+
+        int i = 0, j = 0;
+
+        while (i < N && j < M) {
+            while (i < N && S.charAt(i) == '#') i++;
+            while (j < M && T.charAt(j) == '#') j++;
+
+            if (i == N || j == M) break;
+
+            if (S.charAt(i) == 'A') {
+                if (T.charAt(j) == 'A') {
+                    if (j > i) return 0;
+                    else i++; j++;
+                }
+                else return 0;
+            }
+            else {
+                if (T.charAt(j) == 'B') {
+                    if (j < i) return 0;
+                    else i++; j++;
+                }
+                else return 0;
+            }
+        }
+
+        return 1;
+    }
+}
