86b1c77a713ecff904dd45f0128c4fb6c44ad764
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2023-07-05 23:17:25 +0530

    Create Stock buy and sell II.java

---
+++ src/Stock buy and sell II.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] prices = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.stockBuyAndSell(n, prices);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int stockBuyAndSell(int n, int[] prices) {
+        int x = n-1;
+        int y = n-1;
+        int max = 0;
+        for(int i =n-1; i>0; i--){
+            if(prices[i]<prices[i-1]){
+                y=i;
+                max+= prices[x]-prices[y];
+                x=y-1;
+            }
+            if(i==1){
+                y=0;
+                max+= prices[x]-prices[y];
+            }
+        }
+        return max;
+    }
+} 
