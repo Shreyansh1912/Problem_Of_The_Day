f5df210dad63a6872954396a08bb9184c197c9ef
Shreyansh1912 <98804905+Shreyansh1912@users.noreply.github.com>
2023-07-09 13:20:42 +0530

    Create Smallest Positive missing number.java

---
+++ src/Smallest Positive missing number.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+import java.util.*;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    //Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int size)
+    {
+        boolean vis[] = new boolean[size+1];
+        
+        for(int i=0; i<size; i++){
+            if(arr[i] > 0 && arr[i] <= size){
+                vis[arr[i]] = true;
+            }
+        }
+        for(int i=1; i<=size; i++){
+            if(!vis[i]){
+                return i;
+            }
+        }
+        return size+1;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class Main
+{   
+    public static void main (String[] args) 
+    {
+
+		Scanner sc=new Scanner(System.in);
+		
+        //taking testcases
+		int t=sc.nextInt();
+		while(t-->0){
+		    
+		    //input number n
+			int n=sc.nextInt();
+			int[] arr=new int[n];
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i]=sc.nextInt();
+				
+			Solution obj = new Solution();
+			
+			//calling missingNumber()
+			int missing = obj.missingNumber(arr,n);
+			System.out.println(missing);
+		}
+    }
+}
+
+
+// } Driver Code Ends
